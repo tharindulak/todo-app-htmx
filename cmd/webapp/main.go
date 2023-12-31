b440b29f9d1f4a59a94546e0bd13b34053376089
CharukaK <charukakarunanayake@gmail.com>
2023-12-31 11:04:00 +0530

    Add chi for routing

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	"github.com/charukak/todo-app-htmx/pkg/server"
 	"github.com/charukak/todo-app-htmx/internal/handlers"
+	"github.com/charukak/todo-app-htmx/pkg/server"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
 	s := server.NewServer()
 	h := handlers.NewHandler()
 	s.HandleStatic("/static/", "./static")
 	s.Handle("/", h.Hello)
 	s.Start()
+
 }
 
