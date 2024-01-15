3507419a35c16de9ac82d110d7f06167a4495438
CharukaK <charukakarunanayake@gmail.com>
2024-01-15 09:58:41 +0530

    Add DB creationg logic

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"database/sql"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const (
+	DB_NAME = "todo.db"
+	DB_PATH = "./" + DB_NAME
+)
+
 func main() {
-    db, err := sql.Open("sqlite3", "./foo.db")
+	createDBIfNotExist()
+	db := openDB()
+
+	createTableIfNotExist(db)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -15,4 +26,36 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on
+}
+
+func createDBIfNotExist() {
+	if _, err := os.Stat(DB_PATH); os.IsNotExist(err) {
+		file, err := os.Create(DB_PATH)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+	}
+}
+
+func openDB() *sql.DB {
+	db, err := sql.Open("sqlite3", DB_PATH)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func createTableIfNotExist(db *sql.DB) {
+	sqlStmt := `
+	create table if not exists foo (id integer not null primary key, name text);
+	delete from foo;
+	`
+	_, err := db.Exec(sqlStmt)
+
+	if err != nil {
+		panic(err)
+	}
 }
